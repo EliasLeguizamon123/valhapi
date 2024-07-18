8ecef69e0d0fc0c3198bc4733ab46a001c211376
Elias Leguizamon <elias.leguizamon1997@gmail.com>
2024-07-18 13:13:34 -0300

    feat(installer): generate exe without terminal

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
 from datetime import datetime
+from logging.config import dictConfig
+import os, uvicorn
 
 app = FastAPI()
 
@@ -9,3 +11,58 @@ def get_current_time():
     return { "time": current_time } 
 
 
+# Run with pyinstaller
+DEFAULT_PORT = 12358
+
+# Configurar el logger para escribir en un archivo
+log_filename = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'app.log')
+
+log_config = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "default": {
+            "fmt": "%(levelprefix)s %(asctime)s - %(message)s",
+            "datefmt": "%Y-%m-%d %H:%M:%S",
+            "use_colors": None
+        },
+        "access": {
+            "fmt": '%(levelprefix)s %(asctime)s - %(client_addr)s - "%(request_line)s" %(status_code)s',
+            "datefmt": "%Y-%m-%d %H:%M:%S"
+        },
+    },
+    "handlers": {
+        "default": {
+            "formatter": "default",
+            "class": "logging.FileHandler",
+            "filename": log_filename,
+            "mode": "a",
+        },
+        "access": {
+            "formatter": "access",
+            "class": "logging.FileHandler",
+            "filename": log_filename,
+            "mode": "a",
+        },
+    },
+    "loggers": {
+        "": {
+            "handlers": ["default"],
+            "level": "INFO",
+        },
+        "uvicorn.error": {
+            "level": "INFO",
+        },
+        "uvicorn.access": {
+            "handlers": ["access"],
+            "level": "INFO",
+            "propagate": False,
+        },
+    },
+}
+
+dictConfig(log_config)
+
+if __name__ == "__main__":
+    port = int(os.getenv("PORT", DEFAULT_PORT))
+    uvicorn.run("main:app", host="0.0.0.0", port=port, log_config=log_config)
