06644fd94d80ac03594fd84a1765a69724e0164c
Elias Leguizamon <elias.leguizamon1997@gmail.com>
2024-08-24 11:14:23 -0300

    remove virtual printers from printers endpoint

--- app/routes/printers.py
+++ app/routes/printers.py
@@ -6,4 +6,9 @@ router = APIRouter()
 @router.get("/")
 def get_printers():
     printers = [printer[2] for printer in win32print.EnumPrinters(2)]
+    
+    # Exclude all the virtual printers
+    printers = [printer for printer in printers if not printer.startswith("Microsoft XPS")]
+    printers = [printer for printer in printers if not printer.startswith("OneNote")]
+    printers = [printer for printer in printers if not printer.startswith("Fax")]
     return {"printers": printers}
